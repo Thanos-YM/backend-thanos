12f6b99c731904ac6f6f602f6caa6739a9c4325e
manbron236 <minjuncho@naver.com>
2025-12-08 18:05:06 +0900

    first commit

---
+++ src/main/java/com/thanos/backend_thanos/BackendThanosApplication.java
@@ -0,0 +1,13 @@
+package com.thanos.backend_thanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendThanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendThanosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thanos/backend_thanos/BackendThanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanos.backend_thanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendThanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
